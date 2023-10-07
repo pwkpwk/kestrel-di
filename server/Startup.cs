11e0b0ed9b0d7add7ecc5919fe9f250274501eef
Pavel Karpenko <pwkpwk@hotmail.com>
2023-10-07 01:07:11 -0700

    Put Startup in a namespace

--- server/Startup.cs
+++ server/Startup.cs
@@ -1,26 +1,29 @@
 ﻿using com.ambientbytes.kestreldi.server.Services;
 
-public class Startup
+namespace com.ambientbytes.kestreldi.server
 {
-    private IConfiguration Configuration { get; init; }
-
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-        Configuration = configuration;
-    }
+        private readonly IConfiguration _configuration;
 
-    public void Configure(IApplicationBuilder builder)
-    {
-        builder.UseRouting();
-        builder.UseEndpoints(endpoints => endpoints.MapControllers());
-    }
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder builder)
+        {
+            builder.UseRouting();
+            builder.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
 
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddControllers();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
 
-        services.AddScoped<IServiceOne, ServiceOne>();
-        services.AddScopedLazy<IServiceOne>();
-    }
+            services.AddScoped<IServiceOne, ServiceOne>();
+            services.AddScopedLazy<IServiceOne>();
+        }
+    }    
 }
